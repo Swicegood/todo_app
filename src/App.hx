b52bf818d11d7e654a6b891aafe1a056338f30e0
bkg-academy <bkg-academy@bkgacademy-ThinkCentre-M81>
2025-02-10 14:31:30 -0500

    move to haxe react

---
+++ src/App.hx
@@ -0,0 +1,40 @@
+package src;
+import react.React;
+import react.ReactDOM;
+import react.ReactMacro.jsx;
+import js.Browser.document;
+import react.ReactComponent;
+
+typedef Item = {
+	var name : String;
+	var done : Bool;
+}
+
+class App extends ReactComponent {
+
+	public var items: List<Item>;
+
+	static public function main() {
+		ReactDOM.render(
+			jsx('<App/>'),
+			document.getElementById('root')
+		);
+	}
+
+	override function render() {
+		var cname = 'it-bops';
+		return jsx('
+			<div className={cname}>
+				<h1>Hello React</h1>
+				<newItem/>
+			</div>
+		');
+	}
+
+	function newItem() {
+		return jsx('
+			<input name ="new item" />
+		');
+	}
+	
+}
