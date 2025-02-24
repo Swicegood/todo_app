d17c25b16239ce0dea1f947555c1d518c607c5ba
bkg-academy <bkg-academy@bkgacademy-ThinkCentre-M81>
2025-02-24 16:01:27 -0500

    migration

--- src/App.hx
+++ src/App.hx
@@ -5,42 +5,21 @@ import react.ReactMacro.jsx;
 import js.Browser.document;
 import react.ReactComponent;
 
-typedef Item = {
-	var name : String;
-	var done : Bool;
-}
-
 class App extends ReactComponent {
 
-	public var items: List<Item>;
+    static public function main() {
+        ReactDOM.render(
+            jsx('<App />'),
+            document.getElementById('root')
+        );
+    }
 
-	static public function main() {
-		ReactDOM.render(
-			jsx('<App/>'),
-			document.getElementById('root')
-		);
-	}
-
-	override function render() {
-		var cname = 'it-bops';
-		return jsx('
-			<div className={cname}>
-				<h1>Hello React</h1>
-				<newItem/>
-			</div>
-		');
-	}
-
-	function renderItems(){
-		return jsx('
-		<p></p>
-		');
-	}
-
-	function newItem() {
-		return jsx('
-			<input name ="new item" />
-		');
-	}
-	
+    override function render() {
+        var cname = 'it-bops';
+        return jsx('
+            <div className={cname}>
+                <h1>Hello React</h1>
+            </div>
+        ');
+    }
 }
